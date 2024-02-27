5251fcbbea22bb0f5eb57014b4e16763b1dd1653
minjiyooh <opozz11@naver.com>
2024-02-27 11:10:29 +0900

    first Project

---
+++ j0226/src/t01/Test3.java
@@ -0,0 +1,7 @@
+package t01;
+
+class Test3 { //클래스 첫 글자는 대문자로
+	public static void main(String[] args) {
+		System.out.println("길동이네 집에 오신걸 환영합니다!");
+	}
+}
---
+++ j0226/src/t01/Test4.java
@@ -0,0 +1,20 @@
+package t01;
+
+public class Test4 {
+	//메인 메소드
+	public static void main(String[] args)  { // * 메소드 블럭 메인을 메소드 - 실행메소드 string은 문자열
+		//콘솔에 내용 출력 :sysout
+		System.out.println("1.반갑\t습니다."); //\t는 탭,\n은 엔터키 (줄바꾸기)
+		System.out.print("2.반갑습니다.\n\n");
+		System.out.println("3.반갑습니다.");
+		
+		/*
+		  이곳은 여러줄의 주석처리
+		  여러줄에 설명을 기술할 수 있습니다.
+		 */
+		
+		System.out.println("3.반갑습니다.");
+	
+	}
+
+}
